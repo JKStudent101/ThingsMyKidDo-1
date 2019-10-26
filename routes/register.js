898289d8fda7b80b10c091f581ffc515e2161628
Jonathan <jkwong16@my.bcit.ca>
2019-10-26 14:54:16 -0700

    Added and changed new modals
    - Added a new modal
    - changed the kid profile in the registration modal.

---
+++ routes/register.js
@@ -0,0 +1,9 @@
+var Username = ""
+var phone = ""
+var email = ""
+var password = ""
+var nickname = ""
+var gender = ""
+
+var bfname = document.getElementById("myBtn").value;
+document.getElementById("demo").innerHTML = bfname;
