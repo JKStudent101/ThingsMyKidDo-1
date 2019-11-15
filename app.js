7d648b27bc55f9b6ca3ae7241f2e02e2fd271acd
aiibaby <gzq1030@yahoo.com>
2019-11-14 23:05:01 -0800

    fix

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ const request = require('request');
 // import event routes
 const event = require('./routes/event');
 const addevent = require('./routes/addevent');
-const wishlist = require('./routes/wishlist')
+const wishlist = require('./routes/wishlist');
+const profile = require('./routes/profilepage')
 var db = require('./routes/database').init();
 
 app.set('view engine', 'hbs');
@@ -42,6 +43,7 @@ app.use(
 app.use('/event', event);
 app.use('/addevent', addevent);
 app.use('/savewishlist', wishlist);
+app.use('/profile', profile)
 const server = require('http').createServer(app);
 hbs.registerPartials(__dirname + '/views/partials');
 
@@ -197,106 +199,6 @@ app.post('/registerVendor', (req, res) => {
         })
     })
 
-});
-
-app.get('/profile/', (req, res) => {
-    if (!req.cookies.i) {
-        res.redirect('/login')
-    } else {
-        let user_id = req.session.user.user_id;
-        var sql_select_wishlist = 'select wishlist from child where parent_id = ?';
-        db.query(sql_select_wishlist, user_id, (err, result) => {
-            if (result.length > 0) {
-                let sql =
-                        'SELECT DISTINCT child_nickname as nickname\n' +
-                        'FROM child \n' +
-                        'WHERE parent_id =' + user_id ;
-                db.query(sql, (err, result) => {
-                    if (err) {
-                        throw err;
-                    } else {
-                        let data = [];
-                        for (var i = 0; i < result.length; i++) {
-                            data.push(result[i]);
-                        }
-                        res.render('profile.hbs', {
-                            data: data,
-                            user_type: req.session.user.user_type,
-                            vendor_id: req.session.user.user_id
-                        });
-                    }
-                });
-            }else{
-                res.render('profile.hbs',{
-                    user_type: req.session.user.user_type,
-                    vendor_id: req.session.user.user_id
-                });
-            }
-        })
-
-    }
-});
-
-app.get('/profile/:nickname', (req, res) => {
-    if (!req.cookies.i || !req.session.user) {
-        res.redirect('/logout')
-    } else if (req.session.user.user_type != 'parent') {
-        res.redirect('/logout')
-    } else {
-        let nickname = req.params.nickname
-        let user_id = req.session.user.user_id;
-        let sql_array = [user_id, nickname]
-        let sql_select_wishlist = 'select wishlist from child where parent_id = ? AND child_nickname = ?';
-        let data = [];
-        let events = [];
-        let event_sql =
-                'select e.*, t.name as category, v.name as vendorname from event as e \n' +
-                'inner join event_tags as et on e.event_id = et.event_id \n' +
-                'inner join vendor as v on e.vendor_id = v.user_id \n' +
-                'inner join tags as t on et.tag_id = t.tag_id;';
-        db.query(sql_select_wishlist, sql_array, (err, result) => {
-            console.log(result[0].wishlist)
-            if (result[0].wishlist != null) {
-                let wishlist_array = result[0].wishlist.split(",")
-                let name_sql =
-                        'SELECT DISTINCT child_nickname as nickname\n' +
-                        'FROM child \n' +
-                        'WHERE parent_id =' + user_id ;
-                db.query(name_sql, (err, result) => {
-                    if (err) {
-                        throw err;
-                    } else {
-                        for (var i = 0; i < result.length; i++) {    
-                            data.push(result[i]);
-                        }
-
-                        db.query(event_sql, (err, result) => {
-                            if (err) {
-                                throw err;
-                            } else {
-                                for (var i = 0; i < result.length; i++) {
-                                    let event_id = result[i].event_id;
-                                    if (wishlist_array.includes(String(event_id))) {
-                                        events.push(result[i]);
-                                    }
-                                }
-                                res.render('profile.hbs', {
-                                    data: data,
-                                    events: events,
-                                    nickname: nickname
-                                });
-                            }
-                        });
-                          
-                    }
-                });
-
-            }else{
-                res.render('profile.hbs',{});
-            }
-        })
-    }
-    
 });
 
 app.get('/admin', (req,res)=>{
