5a1ecfa74d70178b5f2fdabd324c05b1f18b8dd0
ElephantOfKnowledge <47584249+ElephantOfKnowledge@users.noreply.github.com>
2019-11-26 01:29:43 -0800

    changed notification button to slider on homepage

--- build/notifications.js
+++ build/notifications.js
@@ -1,22 +1,4 @@
 /*
-Sends a push notification from the test notifications endpoint texbox
-*/
-const send = () => {
-    // console.log("Sending xml request");
-    var request = new window.XMLHttpRequest();
-    let text = document.getElementById('txt').value;
-    let data = {
-        message: text
-    }
-    request.open('post', '/text-me', false);
-    request.setRequestHeader('Content-Type', 'application/json');
-    request.send(JSON.stringify(data));
-    // console.log("request sent");
-    // let response = JSON.parse(request.response);
-    // console.log(response)
-}
-
-/*
 Registers a service worker
 */
 const registerServiceWorker = async () => {
@@ -63,7 +45,6 @@ async function openPushSubscription() {
                     },
                     body: JSON.stringify(PushSubscription),
                 })
-                displayNoButton();
                 response.json().then(res => console.log(res.message))
             }
         } else {
@@ -89,7 +70,6 @@ async function closePushSubscription() {
             },
             body: JSON.stringify(subscription),
         })
-        displayYesButton();
         response.json().then(res => console.log(res.message))
         // console.log("Unsubscribed", successful);
     } catch (e) {
@@ -98,36 +78,19 @@ async function closePushSubscription() {
     };
 }
 
-const displayYesButton = () => {
-    let button = document.getElementById('permission-btn-yes');
-    if (button) {
-        button.style.display = 'block'
-        button = document.getElementById('permission-btn-no');
-        button.style.display = 'none'
-    }
-
-}
-
-const displayNoButton = () => {
-    let button = document.getElementById('permission-btn-no');
-    if (button) {
-        button.style.display = 'block'
-        button = document.getElementById('permission-btn-yes');
-        button.style.display = 'none'
-    }
-}
-
-navigator.serviceWorker.ready.then((registration) => {
-    registration.pushManager.getSubscription().then(subscription => {
-        if (subscription === null) {
-            displayYesButton();
+if (document.getElementById("notif-check")){
+    console.log('element exists')
+    let checkbox = document.getElementById("notif-check");
+    checkbox.addEventListener( 'change', function() {
+        if(this.checked) {
+            console.log('checked');
+            openPushSubscription();
         } else {
-            displayNoButton();
+            console.log('unchecked');
+            closePushSubscription();
         }
-    })
-});
-
-
+    });
+}
 
 
 
