76bb760227e665fbfa5cf87043dded305bf4bac7
aiibaby <gzq1030@yahoo.com>
2019-11-29 00:39:39 -0800

    continue to change profile page

--- build/profile.js
+++ build/profile.js
@@ -1,12 +1,8 @@
-document.getElementById('selectdiv').click
-
-
 
 document
     .getElementById('target')
     .addEventListener('change', function() {
         let nickname = this.value
-        console.log(window.location.origin)
         window.location.replace(window.location.origin + "/profile/" + nickname) 
     });
 
